2d73e60b12a9acb9745e3a9bf2e4f5622ab19a91
GAMch1k <gammail404@gmail.com>
2024-10-11 21:48:24 +0200

    database stuff and sending message

--- daatbase.js
+++ daatbase.js
@@ -1,7 +1,8 @@
 const { MongoClient } = require("mongodb");
+require('dotenv').config()
 
-const uri = "<connection string uri>";
-
+const uri = process.env.DB_STRING;
+// console.log(uri)
 const client = new MongoClient(uri);
 
 async function run() {
@@ -17,4 +18,24 @@ async function run() {
         await client.close();
     }
 }
-run().catch(console.dir);
+// run().catch(console.dir);
+
+async function info(){
+    const database = client.db("drawings_bot")
+    const collection = database.collection("users")
+    return await collection.count()
+}
+
+async function getUsers() {
+    const database = client.db("drawings_bot")
+    const collection = database.collection("users")
+
+    let cur = await collection.find({}, {})
+    let res = await cur.toArray()
+    return res
+}
+
+module.exports = {
+    info,
+    getUsers,
+}
--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const TelegramBot = require('node-telegram-bot-api');
-
+const db = require("./daatbase.js")
 require('dotenv').config()
 
 const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
+    const text = msg.text
 
-    bot.sendMessage(chatId, 'Received your message');
+    if(text == "/info") {
+	let info = await db.info()
+	await bot.sendMessage(chatId, info)
+	return
+    }
+
+    let users = await db.getUsers()
+
+    users.forEach(async(user) => {
+	try {
+	    await bot.copyMessage(user.userId, chatId, msg.message_id)
+	} catch (err) { console.log(err) }
+	//console.log(user.userId)
+	//bot.copyMessage(user.userId, chatId, msg.message_id)
+
+    })
+   // console.log(msg)
+    // bot.copyMessage(532666364, chatId, msg.message_id)
+
+    //bot.sendMessage(chatId, 'Received your message');
 });
 
 
