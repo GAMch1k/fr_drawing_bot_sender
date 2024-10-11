5d02f4257e11c61af5121a48ea2a601af8dae622
GAMch1k <gammail404@gmail.com>
2024-10-11 15:12:50 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+require('dotenv').config()
+
+const token = process.env.TOKEN;
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'Received your message');
+});
+
+
