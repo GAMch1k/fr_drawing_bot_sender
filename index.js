86db960c8eda19d9ffb7894dab29b3b241048b5e
GAMch1k <gammail404@gmail.com>
2024-10-19 02:14:36 +0200

    finished

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const db = require("./daatbase.js")
+
+
 require('dotenv').config()
 
 const token = process.env.TOKEN;
@@ -11,25 +13,20 @@ bot.on('message', async (msg) => {
     const text = msg.text
 
     if(text == "/info") {
-	let info = await db.info()
-	await bot.sendMessage(chatId, info)
-	return
+        let info = await db.info()
+        await bot.sendMessage(chatId, info)
+        return
     }
 
     let users = await db.getUsers()
 
     users.forEach(async(user) => {
-	try {
-	    await bot.copyMessage(user.userId, chatId, msg.message_id)
-	} catch (err) { console.log(err) }
-	//console.log(user.userId)
-	//bot.copyMessage(user.userId, chatId, msg.message_id)
-
+        try {
+            await bot.copyMessage(user.userId, chatId, msg.message_id)
+        } catch{}
     })
-   // console.log(msg)
-    // bot.copyMessage(532666364, chatId, msg.message_id)
 
-    //bot.sendMessage(chatId, 'Received your message');
+    console.log("finished sending")
 });
 
 
